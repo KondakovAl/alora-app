dd65d24d07ea5991e4ac2cc6ffd3d9176959294d
KondakovAl <99503086+KondakovAl@users.noreply.github.com>
2022-07-30 18:09:58 +0500

    Create Typescript template/Section Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+/* scss */
+import './assets/scss/index.scss';
+
+/* images */
+import { logo, SocialFirst, SocialSecond } from './images/index';
+
+
+const data = {
+  header: [
+    {headerNavigation:
+      [
+        {name: 'Work', href: ''},
+        {name: 'Services', href: ''},
+      ]},
+    {headerContacts:
+      [
+        {name: '+1123 456 7890', href: 'tel:+1123 456 7890'},
+        {name: 'hello@medium.com', href: 'mailto:hello@medium.com'},
+        {name: `Let's talk`, href: ''},
+    ]},
+    {headerSocials:
+      [
+        {icon: '', href: ''},
+        {icon: '', href: ''},
+    ]}
+  ]
+}
+
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='header'>
+      <div className="header__wrapper wrapper">
+        <div className="header__logo-container">
+          <img className="header__logo" src={logo}></img>
+        </div>
+        <div className="header__menu">
+          <nav className="header__navigation">
+            <a className="header__navigation-item">Work</a>
+            <a className="header__navigation-item">Services</a>
+          </nav>
+          <div className="header__contacts">
+            <a className="header__contacts-item">+1123 456 7890</a>
+            <a className="header__contacts-item">hello@medium.com</a>
+            <a className="header__contacts-item">Let's talk</a>
+          </div>
+          <div className="header__socials">
+            <a className="header__socials-logo-container">
+            <SocialFirst fill='#373C42'/>
+            </a>
+            <a className="header__socials-logo-container">
+            <SocialSecond fill='#373C42'/>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className='layout'>
+      <Header/>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/images/index.ts
@@ -0,0 +1,5 @@
+import logo from '../assets/images/logo.svg';
+import { ReactComponent as SocialFirst} from '../assets/images/social_1.svg';
+import { ReactComponent as SocialSecond} from '../assets/images/social_2.svg';
+
+export { logo, SocialFirst, SocialSecond  };
